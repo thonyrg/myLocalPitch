574c6b720cbaad58b23f0b1e33784ecf6768a6c2
Anthony Goncalves <thonyrg@gmail.com>
2018-03-24 10:00:30 +0000

    First commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,17 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { SearchComponent } from './components/search/search.component';
+import { SearchBarComponent } from './components/search/search-bar/search-bar.component';
+import { SearchResultsComponent } from './components/search/search-results/search-results.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SearchComponent,
+    SearchBarComponent,
+    SearchResultsComponent
   ],
   imports: [
     BrowserModule
